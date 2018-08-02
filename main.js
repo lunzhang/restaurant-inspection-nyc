21aea7d25f0f05369dd5ea277deb502f92412b2d
lunzhang <lun.zhang1993@gmail.com>
2018-08-02 16:48:59 -0400

    Update jquery

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ let savedRestaurants = [];
 
 const buildRestaurantList = (restaurants, element) => {
   restaurants.forEach(restaurant => {
-    element.append(`<div>${restaurant}</div>`);
+    element.append(`
+    <div>
+      ${restaurant}
+    </div>`);
   });
 };
 
@@ -10,3 +13,7 @@ chrome.storage.local.get(['restaurants'], function(result) {
   savedRestaurants = result.key ? result.key : [1, 2, 3, 4, 5];
   buildRestaurantList(savedRestaurants, $('#saved-restaurant-list'));
 });
+
+$.ajax({url: "https://data.cityofnewyork.us/resource/9w7m-hzhe.json?dba=SOPHIE'S CUBAN CUISINE", success: function(result){
+  console.log(result);
+}});
