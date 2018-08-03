60c79579bbd6e073b4c6570f1845fe4131af1aee
Meena Balady <mbalady@cbinsights.com>
2018-08-03 11:27:39 -0400

    suggestions + name mapping

--- main.js
+++ main.js
@@ -18,11 +18,11 @@ $(document).ready(() => {
 
   const sortViolations = (a, b) => {
     if (a.inspection_date < b.inspection_date)
-     return 1;
-   if (a.inspection_date > b.inspection_date)
-     return -1;
-   return 0;
-  }
+      return 1;
+    if (a.inspection_date > b.inspection_date)
+      return -1;
+    return 0;
+  };
 
   const extractAddress = (violation) => {
     return `${violation.building} ${violation.street} ${violation.boro}, ${violation.zipcode}`;
@@ -35,7 +35,7 @@ $(document).ready(() => {
       url: `https://data.cityofnewyork.us/resource/9w7m-hzhe.json?$where=DBA%20like%20%27%25${term.toUpperCase()}%25%27`,
       success: function (violations) {
         violations.sort(sortViolations);
-        const restaurants = { };
+        const restaurants = {};
         violations.forEach(violation => {
           if (restaurants[violation.camis]) {
             restaurants[violation.camis].violations.push(violation);
@@ -60,7 +60,7 @@ $(document).ready(() => {
     });
   };
 
-  chrome.storage.local.get(['restaurants'], function(result) {
+  chrome.storage.local.get(['restaurants'], function (result) {
     savedRestaurants = result.key ? result.key : [41541793];
     savedRestaurants.forEach(id => {
       $.ajax({
@@ -84,4 +84,66 @@ $(document).ready(() => {
     });
   });
   $searchBtn.bind('click', search);
+
+
+  const nameMap = {
+    "21 Homes Kitchen": 41398041,
+    "Abace Sushi": 50006214,
+    "Aki Sushi": null,
+    "Al Horno Lean Mexican (47th St)": 50010032,
+    "At Nine Thai": 50076881,
+    "Balade Your Way": 50047762,
+    "Bareburger": 50000763,
+    "BOI Noodle House": 41473278,
+    "Bon Chon Chicken": 50016964,
+    "Cafe China": 50072882,
+    "Chopt Creative Salad Co. (Empire State Building)": 50060390,
+    "Curry Dream": 41132647,
+    "Dallas BBQ": 41173328,
+    "Dig Inn": 41365576,
+    "Dim Sum Palace": 50051234,
+    "Dos Toros Taqueria": 50055078,
+    "Evergreen Shanghai Restaurant": 50014124,
+    "Good Seed Salad & Market": 50061189,
+    "Guy & Gallard": 40951884,
+    "Hale & Hearty Soups": 40959649,
+    "Happy Family Chinese and Japanese Restaurant": 41340002,
+    "Hestia": 50009921,
+    "Hudson Market": null,
+    "Hummus & Pita Co.": 50016104,
+    "Indikitch": 50005876,
+    "Jerusalem Cafe": 41643561,
+    "Joy Curry & Tandoor": 41666656,
+    "Just Salad": 41235038,
+    "Kati Roll Company": 41272891,
+    "Kofoo": null,
+    "Kosher Deluxe": 40825868,
+    "Lenwich": 50049229,
+    "Let's Poke": 50075055,
+    "Little Italy Pizza": 41454913,
+    "Market Crates": 50041709,
+    "Meltshop": 50014853,
+    "Mulberry & Vine": 50047068,
+    "Mee Noodle Shop": 50034050,
+    "Natureworks": 41419773,
+    "Pinkberry - Hell's Kitchen": 50049034,
+    "Pokechan": 50057872,
+    "Piccolo Cafe": 41538666,
+    "Pulled & Chopped BBQ": null,
+    "Riko Peruvian": 50051650,
+    "Room Service": 50015344,
+    "Sophie's Cuban": 50055346,
+    "Tacos Time Square": 50060422,
+    "The Original Fresh Tortillas Grill": 50050765,
+    "Tio Pio": 41232741,
+    "Witchcraft": 50065815,
+  };
+
+  $(function () {
+    $("#restaurant-das").autocomplete({
+      source: Object.keys(nameMap),
+      delay: 0
+    });
+  });
+
 });
